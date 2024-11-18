13ec004b94d0497d0cf4e81b07b07b99ff49479e
Edward Lim <ed.lim199@gmail.com>
2024-11-18 17:50:00 +1100

    done for now

--- get_crypto_prices.py
+++ get_crypto_prices.py
@@ -2,6 +2,8 @@
 main, quick script
 '''
 import os
+import time
+import sys
 import requests
 from dotenv import load_dotenv
 
@@ -9,6 +11,16 @@ def main():
     '''
     main
     '''
+    time_interval = None
+    try:
+        time_interval = int(input("Please specify how often in seconds you'd like to make a request for the prices (it must be more than 10): "))
+        if int(time_interval) < 10:
+            print("time was less than 10s", file=sys.stderr)
+            return 1
+    except ValueError:
+        print("input time was not an integer", file=sys.stderr)
+        return 1
+
     load_dotenv()
     api_key = os.getenv("API_KEY")
     cryptonames_to_id = {
@@ -37,19 +49,23 @@ def main():
     }
     # make the request for prices
     api_url = "https://api.coingecko.com/api/v3/simple/price" 
-    print("coin name,USD,AUD")
-    for k, v in cryptonames_to_id.items():
-        response = requests.get(api_url, params={
-            'ids': v,
-            'vs_currencies': 'usd,aud',
-            'x_cg_demo_api_key': api_key
-        },timeout=10)
 
-        if response.status_code == 200:
-            coin_data = response.json()[cryptonames_to_id[k]]
-            print(f"{k},{coin_data['usd']},{coin_data['aud']}")
+    # one table
+    while True:
+        print("\ncoin name,USD,AUD")
+        for k, v in cryptonames_to_id.items():
+            response = requests.get(api_url, params={
+                'ids': v,
+                'vs_currencies': 'usd,aud',
+                'x_cg_demo_api_key': api_key
+            },timeout=10)
 
-
+            if response.status_code == 200:
+                coin_data = response.json()[cryptonames_to_id[k]]
+                print(f"{k},{coin_data['usd']},{coin_data['aud']}")
+        
+        time.sleep(time_interval)
+       
 
 if __name__ == "__main__":
     main()
